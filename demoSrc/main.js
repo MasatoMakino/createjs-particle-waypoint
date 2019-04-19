35de3c985d8815831b6b1677e1e663ca6ed6073b
MasatoMakino <unagiinu.g@gmail.com>
2019-04-19 15:55:11 +0900

    依存パッケージに追従。demoを3次ベジェ曲線でのパス指定に変更。

--- demoSrc/main.js
+++ demoSrc/main.js
@@ -8,6 +8,76 @@ const onDomContentsLoaded = () => {
   initGUI(generator);
 };
 
+const getPath = () => {
+  return [
+    [199.999692558296, 139.037809861326],
+    [
+      199.999692558296,
+      139.037809861326,
+      216.517501342052,
+      91.0703552349714,
+      260.408864739813,
+      101.478048251449
+    ],
+    [
+      304.306569058069,
+      111.887435228376,
+      302.040473463538,
+      149.898168782486,
+      297.969050788655,
+      167.089794944798
+    ],
+    [
+      293.891253219557,
+      184.314831799952,
+      266.291989537884,
+      213.247676318414,
+      241.405592420215,
+      229.538070494226
+    ],
+    [
+      216.517501342052,
+      245.828464670041,
+      201.360366325658,
+      263.475297677716,
+      199.999692558296,
+      269.360116287618
+    ],
+    [
+      198.644947652665,
+      263.475297677716,
+      183.481883774538,
+      245.828464670041,
+      158.595486656873,
+      229.538070494226
+    ],
+    [
+      133.70739557871,
+      213.247676318414,
+      106.111932139903,
+      184.314831799952,
+      102.030334327938,
+      167.089794944798
+    ],
+    [
+      97.9589002789453,
+      149.898168782486,
+      95.6966160382817,
+      111.887435228376,
+      139.592214337275,
+      101.478048251449
+    ],
+    [
+      183.481883774538,
+      91.0703552349714,
+      199.999692558296,
+      139.037809861326,
+      199.999692558296,
+      139.037809861326
+    ]
+  ];
+};
+
 const initStage = () => {
   //ステージ更新処理
   const updateStage = () => {
@@ -39,26 +109,26 @@ const initWay = stage => {
     shapes.push(getShape(i * 0.125 + 1));
   }
 
-  const points = [[100, 100], [100, 200], [200, 200], [200, 300]];
-  const wayPoint = new ParticleWay(points);
-
-  const way = new createjs.Shape();
-  const g = way.graphics;
-  g.beginStroke("#F0F");
-  g.moveTo(points[0][0], points[0][1]);
-  points.forEach((p, index) => {
-    if (index === 0) {
-      g.moveTo(p[0], p[1]);
-      return;
+  const points = getPath();
+  const shape_ewgc = new createjs.Shape();
+  shape_ewgc.graphics.ss(1).beginStroke("rgba(255,0,0,0.25)");
+  const g = shape_ewgc.graphics;
+  for (let i = 0; i < points.length; i++) {
+    if (i === 0) {
+      g.mt(...points[i]);
+      continue;
     }
-    g.lineTo(p[0], p[1]);
-  });
-  stage.addChild(way);
+    g.bt(...points[i]);
+  }
+  g.ef();
+  stage.addChild(shape_ewgc);
+
+  const wayPoint = new ParticleWay(points);
 
   const generator = new CanvasParticleGenerator(stage, wayPoint, shapes, {
     ease: createjs.Ease.cubicOut
   });
-  generator.setSpeed(333, n * 2);
+  generator.setSpeed(166, n * 8);
   generator.play();
   return generator;
 };
